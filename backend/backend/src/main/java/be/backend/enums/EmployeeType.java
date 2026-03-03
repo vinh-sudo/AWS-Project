c0a0790ed7fb4c6a2e2f5182f8b7321b367de534
MinhCoffee <hominhlanhat123@gmail.com>
2026-03-03 14:24:29 +0700

    fix missing file

--- backend/backend/src/main/java/be/backend/enums/EmployeeType.java
+++ backend/backend/src/main/java/be/backend/enums/EmployeeType.java
@@ -4,16 +4,14 @@ public enum EmployeeType {
     ADMIN,
     MANAGER,
     LINE_LEADER,
-    PLANNER,
     WORKER;
-    
+       
     // Map từ Role sang EmployeeType
     public static EmployeeType fromRole(Role role) {
         return switch (role) {
             case ADMIN -> ADMIN;     
             case MANAGER -> MANAGER;
-            case LINE_LEADER -> LINE_LEADER;
-            case PRODUCTION_PLANNER -> PLANNER;
+            case LINE_LEADER -> LINE_LEADER;           
         };
     }
 }
