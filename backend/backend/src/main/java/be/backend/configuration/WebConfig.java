5136ceb5dc0460efd7a3a5a2733971d707241719
Lam Thien Hai Anh <haianhlam29052005@gmail.com>
2026-03-27 22:06:59 +0700

    Rollback backend to commit, keep frontend unchanged

--- backend/backend/src/main/java/be/backend/configuration/WebConfig.java
+++ backend/backend/src/main/java/be/backend/configuration/WebConfig.java
@@ -24,9 +24,6 @@ public class WebConfig implements WebMvcConfigurer {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173"
         ));
-        config.setAllowedOrigins(List.of(
-                "http://ims.mom"
-        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
